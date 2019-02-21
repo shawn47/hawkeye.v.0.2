ea895bcaeeb12275d467254693f714b2df490163
yongboxiao <yongboxiao@creditease.cn>
2019-02-21 17:26:53 +0800

    add do report

---
+++ script/doReport.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+PYTHONPATH=/usr/local/lib/python3.7/site-packages 
+/usr/local/Cellar/python3/3.7.2/bin/python3 /Users/xiaoyongbo/Documents/projects/hawkeye.v.0.2/Main.py doReport > /Users/xiaoyongbo/Documents/projects/hawkeye.v.0.2/logs/app.log 2>&1 & 
